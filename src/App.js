26ff9540f175a1891585f7f7ecb07fc29f72c2a7
taewoo <rlaxodn322@gmail.com>
2023-08-18 07:45:05 +0900

    fetch(API호출)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function App() {
+//https://jsonplaceholder.typicode.com/comments
+const App = () => {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
 
@@ -38,6 +58,6 @@ function App() {
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
+};
 
 export default App;
