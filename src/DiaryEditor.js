74b1c080d4aca0f3bda7c796e5b81e0649b331e7
taewoo <rlaxodn322@gmail.com>
2023-08-17 15:43:57 +0900

    데이터(추가)

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
   const authorInput = useRef();
   const contentInput = useRef();
   const [state, setState] = useState({
@@ -24,7 +24,13 @@ const DiaryEditor = () => {
       contentInput.current.focus();
       return;
     }
+    onCreate(state.author, state.content, state.emotion);
     alert("저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
   };
   return (
     <div className="DiaryEditor">
